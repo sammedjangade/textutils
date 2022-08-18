910a164be0bb618e0f42814158a9dc2120f0a44f
sammed <sammedjangade@gmail.com>
2022-08-18 12:18:56 +0530

    Updates

---
+++ precache-manifest.67d1d50872fbb0bdf4c10f175c6ce9a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b84d3057dbd8c86a52ff",
+    "url": "/textutils/static/css/main.250be5d5.chunk.css"
+  },
+  {
+    "revision": "b84d3057dbd8c86a52ff",
+    "url": "/textutils/static/js/main.b84d3057.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/textutils/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/textutils/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "62145ba569f44a4585e3",
+    "url": "/textutils/static/js/runtime~main.62145ba5.js"
+  },
+  {
+    "revision": "f16ea4f3757c11ab5f1c1f2366a03e9f",
+    "url": "/textutils/index.html"
+  }
+];
